56b247ce65dfcb35658b196e019504f254e36ee1
wmcginty <mcgintw@gmail.com>
2020-07-24 20:14:04 -0500

    Initial Commit

---
+++ Notify/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Notify
+//
+//  Created by William McGinty on 7/24/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Notify/NotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotifyApp.swift
+//  Notify
+//
+//  Created by William McGinty on 7/24/20.
+//
+
+import SwiftUI
+
+@main
+struct NotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
